c542817768119bcfe9285122ba450353d9eed0a4
Valien20 <sena.senturk.19@gmail.com>
2021-01-20 23:56:43 +0300

    Add project files.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] isimler=new string [] {"enin","murat","berk"};
+            Console.WriteLine(isimler[2]);
+
+            isimler = new string[5];
+            isimler[4] = "ilker";
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+
+
+            List<string> isimler2 = new List<string> { "enin", "murat", "berk" };
+            Console.WriteLine(isimler2[0]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+            
+        }
+    }
+}
